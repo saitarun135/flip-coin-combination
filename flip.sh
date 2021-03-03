dbb18c0583f454d70d48233a1662c7e3bd8e5c94
saitarun135 <saitarun135@gm.com>
2021-03-03 12:28:39 +0530

    triplet-combination

--- flip.sh
+++ flip.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 read -p "Enter number of times to flip a combination:" no_of_flips
-declare -A doublet_frequency=( 
+declare -A triplet_frequency=( 
 
-                                [HH]=0
-                                [HT]=0
-                                [TH]=0
-                                [TT]=0
+                                [HHH]=0
+                                [HHT]=0
+                                [HTH]=0
+                                [HTT]=0
+											 [THH]=0
+                                [THT]=0
+                                [TTH]=0
+                                [TTT]=0
+
 
                               )
 
@@ -14,30 +19,47 @@ do
     echo -n "Flip-$flip is "
     (( coin_1 = RANDOM % 2 ))
     (( coin_2 = RANDOM % 2 ))
+	 (( coin_3 = RANDOM % 2 ))
+      case $coin_1$coin_2$coin_3 in 
+        000)
+            echo "Heads Heads Heads"
+            (( triplet_frequency[HHH]++ )) 
+            ;;
+        001)
+            echo "Heads Heads Tails"
+            (( triplet_frequency[HHT]++ ))
+            ;;
+        010)
+            echo "Tails Heads Tails"
+            (( triplet_frequency[THT]++ ))
+            ;;
+        011)
+            echo "Heads Tails Tails"
+            (( triplet_frequency[HTT]++ ))
+            ;;
+			100)
+            echo "Tails Heads Heads"
+            (( triplet_frequency[THH]++ )) 
+            ;;
+        101)
+            echo "Tails Heads Tails"
+            (( triplet_frequency[THT]++ ))
+            ;;
+        110)
+            echo "Tails Tails Heads"
+            (( triplet_frequency[TTH]++ ))
+            ;;
+        111)
+           echo "Tails Tails Tails"
+            (( triplet_frequency[TTT]++ ))
+            ;;    
 
-      case $coin_1$coin_2 in 
-        00)
-            echo "Heads Heads"
-            (( doublet_frequency[HH]++ )) 
-            ;;
-        01)
-            echo "Heads Tails"
-            (( doublet_frequency[HT]++ ))
-            ;;
-        10)
-            echo "Tails Heads"
-            (( doublet_frequency[TH]++ ))
-            ;;
-        11)
-            echo "Tails Tails"
-            (( doublet_frequency[TT]++ ))
-            ;;
-    esac
+esac
 done
 
-for combination in ${!doublet_frequency[@]}
+for combination in ${!triplet_frequency[@]}
 do
-    percentage=$(( ${doublet_frequency[$combination]}  * 100 / no_of_flips ))
-    doublet_frequency[$combination]=$percentage
-    echo "percentage of $combination is ${doublet_frequency[$combination]}%"   
+    percentage=$(( ${triplet_frequency[$combination]}  * 100 / no_of_flips ))
+    triplet_frequency[$combination]=$percentage
+    echo "percentage of $combination is ${triplet_frequency[$combination]}%"   
 done
